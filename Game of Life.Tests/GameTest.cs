526d24eb2f5d4dcf97cfa9a313466fb627da962c
Aleksbgbg <kozichka01@abv.bg>
2020-02-21 08:13:27 +0000

    [Game] Implement fundamental rules of game of life.

---
+++ Game of Life.Tests/GameTest.cs
@@ -0,0 +1,173 @@
+﻿namespace Game.Tests
+{
+    using Xunit;
+
+    public class GameTest
+    {
+        [Fact]
+        public void AliveCell_ZeroNeighbours_Dies()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 0;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.False(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_OneNeighbour_Dies()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 1;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.False(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_TwoNeighbours_Survives()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 2;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.True(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_ThreeNeighbours_Survives()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 3;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.True(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_FourNeighbours_Dies()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 4;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.False(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_FiveNeighbours_Dies()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 5;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.False(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_SixNeighbours_Dies()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 6;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.False(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_SevenNeighbours_Dies()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 7;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.False(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void AliveCell_EightNeighbours_Dies()
+        {
+            const bool middleCellAlive = true;
+            const int aliveNeighbours = 8;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.False(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        [Fact]
+        public void DeadCell_ThreeNeighbours_BecomesAlive()
+        {
+            const bool middleCellAlive = false;
+            const int aliveNeighbours = 3;
+            var grid = Setup3x3Grid(middleCellAlive, aliveNeighbours);
+            Game game = SetupGame(grid);
+
+            game.Evolve();
+
+            Assert.True(grid.IsCellSet(new Point(1, 1)));
+        }
+
+        private static IToggleGrid Setup3x3Grid(bool middleCellIsAlive, int aliveCellsSurroundingMiddle)
+        {
+            var grid = new ToggleGrid2dArray(new Size(3, 3));
+
+            if (middleCellIsAlive)
+            {
+                grid.SetCell(new Point(1, 1));
+            }
+
+            Point[] points =
+            {
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(2, 0),
+                new Point(0, 1),
+                new Point(2, 1),
+                new Point(0, 2),
+                new Point(1, 2),
+                new Point(2, 2),
+            };
+
+            int index = -1;
+
+            for (int i = 0; i < aliveCellsSurroundingMiddle; ++i)
+            {
+                grid.SetCell(points[++index]);
+            }
+
+            return grid;
+        }
+
+        private static Game SetupGame(IToggleGrid grid)
+        {
+            return new Game(grid, new ToggleGrid2dArray(grid.Size));
+        }
+    }
+}
---
+++ Game of Life/Game.cs
@@ -0,0 +1,124 @@
+﻿namespace Game
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class Game
+    {
+        private readonly IToggleGrid _readGrid;
+
+        private readonly IToggleGrid _writeGrid;
+
+        private readonly Size _gridSize;
+
+        private Point _currentPoint;
+
+        public Game(IToggleGrid dataGrid, IToggleGrid sandboxGrid)
+        {
+            Debug.Assert(dataGrid.Size.Equals(sandboxGrid.Size));
+
+            _readGrid = dataGrid;
+            _writeGrid = sandboxGrid;
+            _gridSize = _readGrid.Size;
+        }
+
+        public void Evolve()
+        {
+            for (int x = 0; x < _gridSize.Width; ++x)
+            {
+                for (int y = 0; y < _gridSize.Height; ++y)
+                {
+                    _currentPoint = new Point(x, y);
+                    EvolveCurrentCell();
+                }
+            }
+
+            CopyWriteGridToReadGrid();
+        }
+
+        private void EvolveCurrentCell()
+        {
+            int aliveNeighbours = CountAliveNeighboursAroundCurrentCell();
+
+            if (CurrentCellIsAlive())
+            {
+                if (aliveNeighbours == 2 || aliveNeighbours == 3)
+                {
+                    SetCurrentCellAlive();
+                }
+                else
+                {
+                    SetCurrentCellDead();
+                }
+            }
+            else
+            {
+                if (aliveNeighbours == 3)
+                {
+                    SetCurrentCellAlive();
+                }
+            }
+        }
+
+        private bool CurrentCellIsAlive()
+        {
+            return CellIsAlive(_currentPoint);
+        }
+
+        private void SetCurrentCellAlive()
+        {
+            _writeGrid.SetCell(_currentPoint);
+        }
+
+        private void SetCurrentCellDead()
+        {
+            _writeGrid.UnsetCell(_currentPoint);
+        }
+
+        private int CountAliveNeighboursAroundCurrentCell()
+        {
+            return PointsAround(_currentPoint).Count(CellIsAlive);
+        }
+
+        private bool CellIsAlive(Point point)
+        {
+            return _readGrid.IsCellSet(point);
+        }
+
+        private static IEnumerable<Point> PointsAround(Point point)
+        {
+            int x = point.X;
+            int y = point.Y;
+
+            yield return new Point(x - 1, y - 1);
+            yield return new Point(x, y - 1);
+            yield return new Point(x + 1, y - 1);
+            yield return new Point(x - 1, y);
+            yield return new Point(x + 1, y);
+            yield return new Point(x - 1, y + 1);
+            yield return new Point(x, y + 1);
+            yield return new Point(x + 1, y + 1);
+        }
+
+        private void CopyWriteGridToReadGrid()
+        {
+            for (int x = 0; x < _gridSize.Width; ++x)
+            {
+                for (int y = 0; y < _gridSize.Height; ++y)
+                {
+                    Point point = new Point(x, y);
+
+                    if (_writeGrid.IsCellSet(point))
+                    {
+                        _readGrid.SetCell(point);
+                    }
+                    else
+                    {
+                        _readGrid.UnsetCell(point);
+                    }
+                }
+            }
+        }
+    }
+}
